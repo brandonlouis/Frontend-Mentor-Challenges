87c3b319da9dafc318598bb92d568141318ab1fd
brandonlouis <44594625+brandonlouis@users.noreply.github.com>
2023-10-06 21:11:05 +0800

    Update Footer.js

--- fem-newsletter/src/components/Footer.js
+++ fem-newsletter/src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
 
     if (!isMobile) {
         return (
-            <Box sx={{color:'darkgray', fontSize:'12px', position:'absolute', bottom:'15px', width:'100vw', textAlign:'center'}}>
+            <Box sx={{color:'darkgray', fontSize:'10px', position:'absolute', bottom:'15px', width:'100vw', textAlign:'center'}}>
                 Challenge by <a style={{color: 'lightgray', fontSize:'10px', textDecoration:'none'}} href="https://www.frontendmentor.io/challenges/newsletter-signup-form-with-success-message-3FC1AZbNrv" target="_blank">Frontend Mentor</a>. 
                 Coded by <a style={{color: 'lightgray', fontSize:'10px', textDecoration:'none'}} href="https://github.com/brandonlouis">Brandon Louis Chia</a>.
             </Box>
